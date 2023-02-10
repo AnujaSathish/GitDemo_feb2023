628b104dfc5b4aa8610c639bcb8471100ec8df1d
anusa <anusath78@gmail.com>
2023-02-10 08:54:00 +0000

    JIRA- commit first time

---
+++ src/test/java/uk/axone/devintest/git/tests/App_tests.java
@@ -0,0 +1,17 @@
+package uk.axone.devintest.git.tests;
+
+import org.junit.Test;
+
+public class App_tests {
+
+    @Test
+    public void test1()
+    {
+        System.out.println("running test1");
+    }
+    @Test
+    public void test2()
+    {
+        System.out.println("Running successfully test2");
+    }
+}
