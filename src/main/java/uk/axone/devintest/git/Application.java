018e04bf67b9b63b98ef5d9dd9d5537918bb7c27
anusa <anusath78@gmail.com>
2023-02-10 09:37:24 +0000

    JIRA 4365 - updated some features

--- src/main/java/uk/axone/devintest/git/Application.java
+++ src/main/java/uk/axone/devintest/git/Application.java
@@ -4,4 +4,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("GIt is cool");
     }
+    private void doSomething()
+    {
+        System.out.println("Do something");
+    }
 }
--- src/test/java/uk/axone/devintest/git/tests/App_tests.java
+++ src/test/java/uk/axone/devintest/git/tests/App_tests.java
@@ -5,13 +5,17 @@ import org.junit.Test;
 public class App_tests {
 
     @Test
-    public void test1()
-    {
+    public void test1() {
         System.out.println("running test1");
     }
+
     @Test
-    public void test2()
-    {
+    public void test2() {
         System.out.println("Running successfully test2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("Running successfully test3");
     }
 }
